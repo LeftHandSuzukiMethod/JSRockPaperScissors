53137e7040d267b7f0ca40e3fd3058c3d2e0627b
Mitchell Ladwig <mitchell.ladwig@gmail.com>
2020-01-18 16:35:01 -0700

    Completed computerPlay function, playRound function and added console log. Added JS script tag to HTML

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,15 +1,33 @@
 
-function computerPlay(play){
-    return 
+function computerPlay(){
+    let gameArray = ['rock', 'paper', 'scissors'];
+    return gameArray[Math.floor(Math.random() * gameArray.length)];
 }
 
 function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == 'rock' && computerSelection == 'paper'){
         return 'You lose! Paper beats rock';
     }else if (playerSelection == 'rock' && computerSelection == 'rock'){
         return 'You tie! You have both selected rock';
     }else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-        return 'You win! Rock beats scissors'
+        return 'You win! Rock beats scissors';
+    }else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+        return 'You lose! Scissors beats paper';
+    }else if (playerSelection == 'paper' && computerSelection == 'paper'){
+        return 'You tie! You have both selected paper';
+    }else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        return 'You win! Paper beats rock';
+    }else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+        return 'You lose! Rock beats scissors';
+    }else if (playerSelection == 'scissors' && computerSelection == 'scissors'){
+        return 'You tie! You have both selected scissors';
+    }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+        return 'You win! Scissors beats paper';
     }
 
 }
+
+const computerSelection = computerPlay()
+const playerSelection = 'RoCk'
+console.log(playRound(playerSelection, computerSelection))
